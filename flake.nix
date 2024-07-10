9384845c401977d50dab5b8cc22c4cb7405f7f54
Jade Lovelace <lix@jade.fyi>
2024-07-10 19:42:53 +0200

    flake.nix: switch to 24.05
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/dd37924974b9202f8226ed5d74a252a9785aedf8' (2024-04-24)
      → 'github:NixOS/nixpkgs/249fbde2a178a2ea2638b65b9ecebd531b338cf9' (2024-07-09)

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   # The sources we depend on.
   #
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
